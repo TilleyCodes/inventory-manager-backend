ca2efdff861b57b4c9fd94c7e804ab9ddb6a03bb
Tilley <you@example.com>
2025-04-23 21:19:33 +1000

    test: added post route test case

--- tests/items.test.js
+++ tests/items.test.js
@@ -7,9 +7,19 @@
 
 // Importing a testing helper function from supertest
 const request = require('supertest');
+const mongoose = require('mongoose');
+const connectDB = require("../src/utils/database");
 
 // Importing the code that we want to test
 const app = require("../src/index");
+
+beforeAll(async () => {
+    await connectDB();
+});
+
+afterAll(async () => {
+    await mongoose.connection.close();
+});
 
 describe('GET /health', () => {
     it('should return status OK', async() => {
@@ -20,3 +30,25 @@ describe('GET /health', () => {
     });
 });
 
+describe('POST /items', () => {
+    test('should create a new item and return it', async () => {
+        const newItem = {
+            name: "Test Monitor",
+            price: 29.99,
+            quantityInStock: 25,
+            category: "Accessories"
+        };
+
+        const response = await request(app)
+        .post('/items')
+        .send(newItem)
+        .expect(201);
+
+        expect(response.body).toMatchObject({
+            name: newItem.name,
+            price: newItem.price,
+            quantityInStock: newItem.quantityInStock,
+            category: newItem.category
+        });
+    });
+});
