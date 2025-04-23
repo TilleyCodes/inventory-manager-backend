f2dcd0fb7700a00409ba15b60d3b2a1b2de1c05c
Tilley <you@example.com>
2025-04-23 20:50:45 +1000

    feat: Created the application, defined Create and read routes, created a simple test function

---
+++ tests/items.test.js
@@ -0,0 +1,22 @@
+// describe("description", () => {
+//     it('some description', async () => {
+//     stmts...
+//      })
+//      expect(something).someMatchers(something);
+// })
+
+// Importing a testing helper function from supertest
+const request = require('supertest');
+
+// Importing the code that we want to test
+const app = require("../src/index");
+
+describe('GET /health', () => {
+    it('should return status OK', async() => {
+        const response = await request(app)
+        .get('/health');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({ status: "OK" });
+    });
+});
+
